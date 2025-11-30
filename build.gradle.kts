9f77654426968f796b17fc5a5b060a76a9a5edee
RODea_L00203120 <L00203120@atu.ie>
2025-11-30 15:08:41 +0000

    Updated J-Unit for dependency problem resolve-again

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     
-    /testImplementation(platform("org.junit:junit-bom:5.14.1"))
+    testImplementation(platform("org.junit:junit-bom:5.14.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
